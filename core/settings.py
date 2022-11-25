c196963eca0ad71924db725caef8fd0e31e3a207
Gaston Franco <gastonfr24@gmail.com>
2022-11-25 15:09:30 -0300

    style.css

--- core/settings.py
+++ core/settings.py
@@ -116,7 +116,7 @@ STATICFILES_DIRS = [
 ]
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 
 
